9caf805f634a31613c1c09fb6ea99c9c354dcf73
DavidCarG <davidzone23@gmail.com>
2022-12-30 13:37:29 -0600

    Make the numbers,operations and results show up on the calculator display

--- script.js
+++ script.js
@@ -32,3 +32,54 @@ function operate(operator,a,b){
     }
 
 }
+
+//Button functions
+let upperDisplay;
+let lowerDisplay = "0";
+let lowerValue = 0;
+let upperValue = 0;
+let operatorBool = false;
+let operator = "";
+
+const upperScreen = document.querySelector('.screen-upper');
+const lowerScreen = document.querySelector('.screen-lower');
+
+function updateDisplay(e){
+    let c = e.target.textContent;
+    if(isNaN(c)){
+        operatorBool=true;
+        if(upperScreen.textContent===""){
+            upperScreen.textContent = lowerValue + " " + c;
+            upperValue = lowerValue;
+            operator = c;
+        }else{
+            if(c==='='){
+                upperScreen.textContent = `${upperValue} ${operator} ${lowerValue} = ${operate(operator,upperValue,lowerValue)}`;
+
+                lowerScreen.textContent = operate(operator,upperValue,lowerValue);
+                operatorBool = false;
+
+            }else{
+                upperValue = operate(operator,upperValue,lowerValue);
+                lowerValue = upperValue;
+
+                upperScreen.textContent = upperValue +" "+c;
+                lowerScreen.textContent = lowerValue;
+
+                operator = c;
+            }
+        }
+
+    }else{
+        if((lowerValue===0 && c!=="0") || operatorBool)
+            lowerScreen.textContent = c;
+        else if(lowerValue>0)
+            lowerScreen.textContent += c;
+        
+        lowerValue = Number(lowerScreen.textContent);
+        operatorBool=false;
+    }
+}
+
+const btnList = Array.from(document.querySelectorAll('.calc-keyboard .row .calc-button.number'));
+btnList.forEach(btn => btn.addEventListener('click',updateDisplay));
